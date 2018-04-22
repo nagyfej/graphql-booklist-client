de7313f3fbb838ed3c11cd2a78b3f23182b12182
Darvas Gábor <nagyfej@gmail.com>
2018-04-22 12:28:42 +0200

    add apollo server, dotenv config for server, get books and add book features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import config from './config/config';
+
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
+const client = new ApolloClient({
+  uri: config.graphQLServerUrl
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninjas Reading List</h1>
-        <BookList></BookList>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninjas Reading List</h1>
+          <BookList></BookList>
+          <AddBook></AddBook>
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const config = {
+  graphQLServerUrl: process.env.REACT_APP_GRAPHQL_SERVER_URL
+};
+
+module.exports = config;
